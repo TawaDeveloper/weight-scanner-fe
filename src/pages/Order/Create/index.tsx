7b5ae0540e87fa01a1bbb902f336acb062630804
lijieQAQ <lijie1319722045@126.com>
2023-10-10 11:23:27 +0800

    fix: pagination false

--- src/pages/Order/Create/index.tsx
+++ src/pages/Order/Create/index.tsx
@@ -147,11 +147,7 @@ const CreateOrder = () => {
       },
     ],
     scroll: { x: 1160 },
-    pagination: {
-      defaultPageSize: 20,
-      pageSize: 20,
-      pageSizeOptions: [10, 20, 50, 100],
-    },
+    pagination: false,
   };
 
   const formProps = useMemo(() => {
